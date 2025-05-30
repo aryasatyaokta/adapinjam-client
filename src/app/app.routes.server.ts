058bc6bab6fa31f95e19475fb22dd012ad7703e9
aryaganteng <hadi.argaya@gmail.com>
2025-05-30 16:11:14 +0700

    update vercel 3

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'reset-password/:token',
+    renderMode: RenderMode.Server  // ⬅️ penting!
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
